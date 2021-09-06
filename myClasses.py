1f9646474c17b0fc63528a77ca69bb5e6dcae6d9
evoclive <ctdarwell@gmail.com>
2021-09-06 08:22:28 +0700

    Update myClasses.py

--- myClasses.py
+++ myClasses.py
@@ -1,6 +1,3 @@
-#this inherits Python dict but reap returns a default instead of get's None type
-# ***ALWAYS*** put in C:\Users\Windows\Anaconda3\Lib\site-packages
-
 #modifies list.remove(), list.index() & provides set() & wildcard()
 class xList(list):
     def __init__(self, *args, **kwargs):
@@ -17,18 +14,18 @@ class xList(list):
     def indxs(self, qw): #yield all indexes - cf. index()
         return xList([x for x in range(len(self)) if self[x] == qw])
 
-    def items(self): #yields xl set
+    def items(self): #yields xList set
         return xList(list(set(self))) #set as a list
     
     def wildcard(self, w): #yield wildcard in list strings
         try: return xList([x for x in self if w in x])
         except: print("Incorrect data type. <None> returned!")
 
-
+#modifies dict.get() to return zero or other user-defined value
 class xDict(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.out = 0
+        self.out = 0 #initial return value
 
     def reap(self, qw):
         if qw not in self.keys(): return self.out
